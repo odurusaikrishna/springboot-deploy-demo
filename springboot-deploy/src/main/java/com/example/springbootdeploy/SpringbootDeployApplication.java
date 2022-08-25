c4c216babf331a1fe74e22e4b0ebbb79fe4cdafe
odurusaikrishna <110734662+odurusaikrishna@users.noreply.github.com>
2022-08-25 19:37:41 +0530

    Add files via upload

---
+++ springboot-deploy/src/main/java/com/example/springbootdeploy/SpringbootDeployApplication.java
@@ -0,0 +1,21 @@
+package com.example.springbootdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDeployApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "Welcome page was done";
+	}
+
+}
